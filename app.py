c6a70050e1d6b461792e24815d304b02e0995c64
dnote <dnote1127@gmail.com>
2025-04-10 17:34:52 +0530

    Updated code for movie rating layout

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def recommend(movie):
     return recommended_movie_names,recommended_movie_posters,recommended_movie_review
 
 
-st.header('Movie Recommender System ')
+st.header('MOVIES RECOMMENDER AND REVIEW')
 movies = pickle.load(open('artifacts/movie_list.pkl','rb'))
 similarity = pickle.load(open('artifacts/similarity.pkl','rb'))
 
@@ -99,8 +99,13 @@ st.link_button("REVIEW", google_form_link)
 
 #UPDATING
 
-st.header('THE LATEST MOVIES')
-st.markdown("<hr style='border: 1px solid gray; width: 80%; margin: auto;'>", unsafe_allow_html=True)
+st.markdown("""
+<div style="text-align: center; margin-bottom: 20px;">
+    <h2 style="margin-bottom: 5px;">THE LATEST MOVIES</h2>
+    <hr style="border: none; height: 2px; background-color: gray; width: 100%;">
+</div>
+""", unsafe_allow_html=True)
+
 
 ### MOVIE 1 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
